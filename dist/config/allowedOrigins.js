57ec255b8ed65cf87b64f85e09554ffa9c801f6b
endryus.henrique <endryus.henrique@rentcars.com>
2023-06-07 08:43:03 -0300

    testing

--- dist/config/allowedOrigins.js
+++ dist/config/allowedOrigins.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const allowedOrigins = ['http://localhost:3000'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
 exports.default = allowedOrigins;
 //# sourceMappingURL=allowedOrigins.js.map
